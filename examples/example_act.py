dda0417cf75431af0568330a95cdf7481c496a1d
Hao Zhuang <hzhuang@google.com>
2019-08-15 15:14:40 -0700

    copybara flow testing
    PiperOrigin-RevId: 263652181
    Change-Id: I730bb0736ee493e4b5ee9c10a7501a7146128a35

--- examples/example_act.py
+++ examples/example_act.py
@@ -14,7 +14,6 @@
 # limitations under the License.
 # ==============================================================================
 """Example the usage of activation functions in qkeras."""
-
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
