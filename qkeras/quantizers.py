Added some fixes by Claudionor: - Fixed multiple instance of tensorflow conditionals in quantizers.py - Changed stochastic_ternary test to remove the 0 variance cases

qkeras/quantizers.py
@@ -662,6 +662,11 @@ class ternary(BaseQuantizer):  # pylint: disable=invalid-name
         thres = self.threshold
       q = K.cast(tf.abs(x) >= thres, K.floatx()) * tf.sign(x)
 
+    # ternary ranges from -1 to +1, so we use tanh(x) to be a differentiable
+    # version of that.
+    if self.alpha is None:
+      x = K.tanh(x)
+
     self.scale = scale
     return x + tf.stop_gradient(-x + scale * q)
 
@@ -754,6 +759,11 @@ class stochastic_ternary(ternary):  # pylint: disable=invalid-name
 
   def __call__(self, x):
     def stochastic_output():
+      # right now we only accept alpha = "auto" or "auto_po2"
+
+      assert isinstance(self.alpha, six.string_types)
+      assert self.alpha in ["auto", "auto_po2"]
+
       if self.alpha is None:
         scale = self.default_alpha
       elif isinstance(self.alpha, six.string_types):
@@ -772,7 +782,6 @@ class stochastic_ternary(ternary):  # pylint: disable=invalid-name
       else:
         axis = [0]
 
-      x_mean = K.mean(x, axis=axis, keepdims=True)
       x_std = K.std(x, axis=axis, keepdims=True)
 
       m = K.max(tf.abs(x), axis=axis, keepdims=True)
@@ -785,8 +794,8 @@ class stochastic_ternary(ternary):  # pylint: disable=invalid-name
         q_ns = K.cast(tf.abs(x) >= T, K.floatx()) * K.sign(x)
         scale = _get_scale(self.alpha, x, q_ns)
 
-      x_norm = (x - x_mean) / x_std
-      T = scale / (2.0 * x_std)
+      x_norm = x / (x_std + K.epsilon())
+      T = scale / (2.0 * (x_std + K.epsilon()))
 
       if self.use_real_sigmoid:
         p0 = tf.keras.backend.sigmoid(self.temperature * (x_norm - T))
@@ -917,25 +926,27 @@ class binary(BaseQuantizer):  # pylint: disable=invalid-name
       m = tf.where(m > 1.0, tf.ones_like(m), m)
       f = 2 * m
 
-      x = f * _round_through(
-          x / f, 
-          use_stochastic_rounding=tf.math.logical_and(
-              tf.cast(self.use_stochastic_rounding, dtype=tf.bool),
-              tf.cast(K.learning_phase(), dtype=tf.bool)),
-          precision=0.125
-      )
+      x = tf_utils.smart_cond(
+          K.learning_phase(),
+          lambda: f * _round_through(
+              x / f, use_stochastic_rounding=True, precision=0.125),
+          lambda: x)
 
     k_sign = tf.sign(x)
-    if tf.math.logical_and(
-          tf.cast(self.use_stochastic_rounding, dtype=tf.bool),
-          tf.cast(K.learning_phase(), dtype=tf.bool)):
-          
-      k_sign += (1.0 - tf.abs(k_sign)) * (
-          2.0 * tf.round(tf.random.uniform(tf.shape(x))) - 1.0)
+    if self.use_stochastic_rounding:
+      # in inference, we use a biased "1" for stochastic rounding right now
+      k_sign += (1.0 - tf.abs(k_sign)) * tf_utils.smart_cond(
+          K.learning_phase(),
+          lambda: 2.0 * tf.round(tf.random.uniform(tf.shape(x))) - 1.0,
+          lambda: tf.ones_like(tf.shape(x), dtype=K.floatx()))
       # if something still remains, just make it positive for now.
     k_sign += (1.0 - tf.abs(k_sign))
     if self.use_01:
       k_sign = (k_sign + 1.0) / 2.0
+
+    # approximate binary by tanh(x) as it has limited range between -1 and +1.
+    if self.alpha is None:
+      x = K.tanh(x)
 
     scale = _get_scale(self.alpha, x, k_sign)
     self.scale = scale
@@ -1490,10 +1501,9 @@ class quantized_po2(BaseQuantizer):  # pylint: disable=invalid-name
     x_sign = tf.sign(x)
     x_sign += (1.0 - tf.abs(x_sign))
     x_abs = tf.abs(x)
-    x_clipped = _clip_power_of_two(x_abs, self._min_exp, self._max_exp,
-                                   self.max_value,
-                                   self.quadratic_approximation,
-                                   tf.math.logical_and(
+    x_clipped = _clip_power_of_two(
+        x_abs, self._min_exp, self._max_exp, self.max_value,
+        self.quadratic_approximation, tf.math.logical_and(
                                       tf.cast(self.use_stochastic_rounding, dtype=tf.bool),
                                       tf.cast(K.learning_phase(), dtype=tf.bool)))
     return x + tf.stop_gradient(-x + x_sign * pow(2.0, x_clipped))
@@ -1589,10 +1599,9 @@ class quantized_relu_po2(BaseQuantizer):  # pylint: disable=invalid-name
       x = tf.where(
           x <= self.max_value, K.relu(x), tf.ones_like(x) * self.max_value)
 
-    x_clipped = _clip_power_of_two(x, self._min_exp, self._max_exp,
-                                   self.max_value,
-                                   self.quadratic_approximation,
-                                   tf.math.logical_and(
+    x_clipped = _clip_power_of_two(
+        x, self._min_exp, self._max_exp, self.max_value,
+        self.quadratic_approximation, tf.math.logical_and(
                                       tf.cast(self.use_stochastic_rounding, dtype=tf.bool),
                                       tf.cast(K.learning_phase(), dtype=tf.bool)))
     return x + tf.stop_gradient(-x + pow(2.0, x_clipped))

tests/qactivation_test.py
@@ -442,48 +442,45 @@ def test_stochastic_binary_inference_mode(alpha, test_values, expected_values):
 
 
 @pytest.mark.parametrize(
-    'bound, alpha, temperature, expected_values, expected_scale',
-    [(0.5, None, 8,
-      np.array([-0.254, -0.028, -0.025, 0., 0., 0., 0.001, 0., 0.02, 0.141
-               ]).astype(np.float32), np.array([1.0])),
-     (0.01, 0.0033, 8,
-      np.array([-1., -0.999, -0.999, -0.735, 0.605, 0.62, 0.972, 0.981, 1., 1.
-               ]).astype(np.float32), np.array([0.0033])),
-     (3, 5.0, 8,
-      np.array([-0.682, -0.135, -0.132, 0., 0., 0., 0.002, 0.003, 0.194, 0.709
-               ]).astype(np.float32), np.array([5.0])),
-     (0.01, 'auto', 8,
-      np.array([
-          -0.999, -0.987, -0.992, -0.219, 0.114, 0.114, 0.697, 0.828, 0.991, 1.
-      ]).astype(np.float32), np.array([0.0068196])),
-     (0.01, 'auto_po2', 8,
-      np.array([
-          -0.998, -0.986, -0.977, -0.127, 0.057, 0.088, 0.631, 0.709, 0.986,
-          0.999
-      ]).astype(np.float32), np.array([0.007812]))])
-def test_stochastic_ternary(bound, alpha, temperature, expected_values,
-                            expected_scale):
+    'bound, alpha, temperature, expected_values, expected_scale', [
+    (
+        0.01,
+        "auto",
+        8,
+        np.array([-0.973, -0.903, -0.759, -0.574, -0.242,  0.161,  0.508,  0.723,
+            0.874,  0.975]).astype(np.float32),
+        np.array([0.008427, 0.007001, 0.0057  , 0.004457, 0.003537, 0.003416,
+            0.004507, 0.005536, 0.006853, 0.008282]).astype(np.float32)
+    ),
+    (
+        0.01,
+        "auto_po2",
+        8, 
+        np.array([-0.979, -0.877, -0.639, -0.586, -0.23 ,  0.154,  0.327,  0.603,
+            0.83 ,  0.986]).astype(np.float32),
+        np.array([0.007812, 0.007812, 0.007812, 0.003906, 0.003906, 0.003906,
+            0.007812, 0.007812, 0.007812, 0.007812]).astype(np.float32)
+    )
+])
+def test_stochastic_ternary(bound, alpha, temperature, expected_values, expected_scale):
   np.random.seed(42)
   K.set_learning_phase(1)
 
-  x = np.random.uniform(-bound, bound, size=10)
-  x = np.sort(x)
+  n = 1000
+
+  x = np.random.uniform(-bound, bound, size=(n, 10))
+  x = np.sort(x, axis=1)
 
   s = stochastic_ternary(alpha=alpha, temperature=temperature)
 
+  y = K.eval(s(K.constant(x)))
+  scale = K.eval(s.scale).astype(np.float32)[0]
+
   ty = np.zeros_like(s)
-  ts = 0.0
-
-  n = 1000
-
-  for _ in range(n):
-    y = K.eval(s(K.constant(x)))
-    scale = float(K.eval(s.scale))
-    ts = ts + scale
-    ty = ty + (y / scale)
+  for i in range(n):
+    ty = ty + (y[i] / scale)
 
   result = (ty/n).astype(np.float32)
-  scale = np.array([ts/n])
 
   assert_allclose(result, expected_values, atol=0.1)
   assert_allclose(scale, expected_scale, rtol=0.1)